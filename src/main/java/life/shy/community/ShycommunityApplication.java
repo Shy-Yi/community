5bfb0d8d243164555973724ef3904316af9a5ffa
Shy-Yi <1148947585@qq.com>
2022-04-03 10:24:24 +0800

    add redeme

---
+++ src/main/java/life/shy/community/ShycommunityApplication.java
@@ -0,0 +1,13 @@
+package life.shy.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShycommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShycommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/shy/community/ShycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.shy.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
